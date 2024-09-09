af0cc26485c390c3c52c0bed35fb67714e0de2e1
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-09-09 23:27:54 +0530

    project setup

--- os_fs.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var os = require('os');
-
-var user = os.userInfo();
-console.log(user);
-console.log(user.username);
-
-console.log('System Uptime: ' + os.uptime() + ' seconds');
-console.log('OS Type: ' + os.type());
